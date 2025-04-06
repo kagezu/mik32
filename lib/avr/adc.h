95532835ada6ca5af5ed15b4c1a22463c72d20ac
hinto <hinto@rikt.ru>
2025-04-06 22:32:11 +0700

    fix adc

---
+++ lib/avr/adc.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "avr.h"
+
+#undef  ADC
+#define ADCW  _SFR_MEM16(0x78)
+
+#define ADC_AREF    0
+#define ADC_AVCC    1
+#define ADC_INT     3
+
+#define ADC_CH_TEMP 8
+#define ADC_CH_VBG  14
+#define ADC_CH_GND  15
+
+#define ADC_ADLAR   1
+
+class ADC_Class {
+public:
+  // Частота в КГц
+  void init(uint8_t ch, uint16_t fq = 0xffff)
+  {
+    uint8_t div = 0;
+    uint16_t max_fq = F_CPU / 26000;
+
+    while (div < 0x07) {
+      if (fq >= max_fq) break;
+      max_fq >>= 1;
+      div++;
+    }
+
+    DIDR0 |= _BV(ch);         // Отключить цифровой вход
+    ADMUX = ch                // Выборать канал
+      | (ADC_AVCC << REFS0);  // AREF = AVCC
+    ADCSRA = (1 << ADEN)      // Включить
+      | (1 << ADSC)           // Выполнить измерение
+      | (div << ADPS0)        // Делитель
+      | (ADC_ADLAR << ADLAR); // Выравнивание 8/10 bit
+    wait();                   // Ждать завершения
+  }
+
+  // Выборать канал
+  void chanel(uint8_t ch) { ADMUX = ch | (ADC_AVCC << REFS0); }
+
+  void single() { ADCSRA |= _BV(ADSC); }
+  void start() { ADCSRA |= _BV(ADSC) | _BV(ADATE); }
+  void stop() { ADCSRA &= ~_BV(ADATE); ADCSRA |= _BV(ADEN); }
+  void wait() { while (!(ADCSRA & _BV(ADIF))); }
+
+#if ADC_ADLAR
+  uint8_t value() { return ADCH; }
+#else
+  uint16_t value() { return ADCW; }
+#endif
+};
+
+
+
+
+
--- lib/mik32/adc.h
+++ lib/mik32/adc.h
@@ -6,18 +6,20 @@
 class ADC {
 public:
 
-  void init(uint8_t chanel, uint16_t delay = 0x0C)
+  void init(uint8_t ch, uint16_t delay = 0x0C)
   {
     PM->CLK_APB_P_SET = PM_CLOCK_APB_P_ANALOG_REGS_M; // Включить тактирование
     ANALOG_REG->ADC_CONFIG =
       (delay << ADC_CONFIG_SAH_TIME_WRITE_S)// Задержка
-      | (chanel << ADC_CONFIG_SEL_S)        // Выборать канал
+      | (ch << ADC_CONFIG_SEL_S)            // Выборать канал
       | ADC_CONFIG_RN_M                     // Сброс
       | ADC_CONFIG_EN_M;                    // Включить
 
     single(); // Выполнить измерение
     wait();   // Ждать завершения
   }
+  void chanel(uint8_t ch) {}
+
 
   void single() { ANALOG_REG->ADC_SINGLE = 1; }
   void start() { ANALOG_REG->ADC_CONTINUOUS = 1; }
